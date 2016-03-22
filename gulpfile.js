1bd2ed269c2fde67358867e23a4f4005a0d8745b
Radu Potop <radu@wooptoo.com>
2016-03-22 11:51:50 +0000

    Build assets

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,9 @@ var assets = {
     'assets/jquery/dist/jquery.min.js',
     'assets/angular/angular.min.js',
     'assets/bootstrap/dist/js/bootstrap.min.js'
+  ],
+  styles: [
+    'assets/bootstrap/dist/css/bootstrap.min.css'
   ]
 };
 
@@ -39,22 +42,19 @@ gulp.task('assets-scripts', function() {
     .pipe(gulp.dest('dist'))
 });
 
-/**
- * Build app scripts
+gulp.task('assets-styles', function() {
+  return gulp.src(assets.styles)
+    .pipe(concat('assets.min.css'))
+    .pipe(gulp.dest('dist'))
+});
+
+/*
+ Build app
  */
 gulp.task('app-scripts', function() {
   return gulp.src(app.scripts)
     .pipe(concat('app.min.js'))
     .pipe(gulp.dest('dist'));
-});
-
-/*
- Build styles
-*/
-gulp.task('assets-styles', function() {
-  return gulp.src(assets.styles)
-    .pipe(concat('assets.min.css'))
-    .pipe(gulp.dest('dist'))
 });
 
 gulp.task('app-styles', function() {
@@ -66,7 +66,13 @@ gulp.task('app-styles', function() {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('server', function(){
+
+gulp.task('assets', ['assets-scripts', 'assets-styles']);
+
+gulp.task('app', ['app-scripts', 'app-styles']);
+
+
+gulp.task('serve', function(){
   gulp.src('')
     .pipe(exec('caddy -conf Caddyfile', {continueOnError: false}));
 });
